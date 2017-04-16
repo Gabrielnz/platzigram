da5488e5a88a3e727a9827f6701b610f59e7c42f
Gabriel Nunez <gabriel.nunez.sme@outlook.com>
2017-04-16 16:34:12 -0400

    14. Agregando JavaScript en el cliente, con Browserify y Babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,44 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
 const rename = require('gulp-rename')
+const browserify = require('browserify')
+const babel = require('babelify')
+const source = require('vinyl-source-stream')
 
 gulp.task('styles', () => {
   gulp
+    // toma el archivo sass
     .src('index.scss')
+    // transforma el archivo
     .pipe(sass())
+    // renombra el archivo a app
     .pipe(rename('app.css'))
+    // genera el archivo en la carpeta public
     .pipe(gulp.dest('public'))
 })
 
 gulp.task('assets', () => {
   gulp
+    // toma todos los assets
     .src('assets/*')
+    // los copia a la carpeta public
     .pipe(gulp.dest('public'))
 })
 
-gulp.task('default', ['styles', 'assets'])
+gulp.task('scripts', () => {
+  // toma el index.js con browserify
+  browserify('./src/index.js')
+    // transforma el archivo usando babel y el preset ES2015
+    .transform(babel, {presets: ["es2015"]})
+    // genera el bundle
+    .bundle()
+    // convierte el bundle resultante en algo que gulp entiende
+    .pipe(source('index.js'))
+    // renombra el bundle a app
+    .pipe(rename('app.js'))
+    // genera el archivo en la carpeta public
+    .pipe(gulp.dest('public'))
+})
+
+// todas las tareas que va a ejecutar gulp
+gulp.task('default', ['styles', 'assets', 'scripts'])

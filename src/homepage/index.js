e2b92a30af30421b5f6a1f207fd8951abc605fc8
Gabriel Nunez <gabriel.nunez.sme@outlook.com>
2017-04-17 19:04:25 -0400

    22. Agregando las imagenes de los usuarios al timeline

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -7,6 +7,28 @@ Page('/', (context, next) => {
   $('title').html('Platzigram')
 
   let main = $('#main-container')
+
+  let pictures = [
+    {
+      user: {
+        username: 'bladelizard',
+        avatar: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
+      },
+      url: 'http://materializecss.com/images/office.jpg',
+      likes: '10',
+      liked: true
+    },
+    {
+      user: {
+        username: 'bladelizard',
+        avatar: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
+      },
+      url: 'http://materializecss.com/images/office.jpg',
+      likes: '3',
+      liked: true
+    }
+  ]
+
   // Limpia el elemento main-container y le inserta el contenido del homepage
-  main.empty().append(homepageTemplate)
+  main.empty().append(homepageTemplate(pictures))
 })
--- src/homepage/template.js
+++ src/homepage/template.js
@@ -1,12 +1,15 @@
 const yo = require('yo-yo')
 const layout = require('../layout')
+const picture = require('../picture-card')
 
-const template = yo`<div class="container timeline">
-  <div class="row">
-    <div class="col s12 m10 offset-m1 l6 offset-l3">
-      content
+module.exports = function (pictures) {
+  const template = yo`<div class="container timeline">
+    <div class="row">
+      <div class="col s12 m10 offset-m1 l6 offset-l3">
+        ${pictures.map((pic) => picture(pic))}
+      </div>
     </div>
-  </div>
-</div>`
+  </div>`
 
-module.exports = layout(template)
+  return layout(template)
+}

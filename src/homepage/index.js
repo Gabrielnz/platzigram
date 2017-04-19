2acf2282e381bc926b8eb76535ac6e43696e6b51
Gabriel Nunez <gabriel.nunez.sme@outlook.com>
2017-04-19 13:54:23 -0400

    30. Obteniendo una respuesta del servidor con Fetch API

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -3,7 +3,7 @@ const homepageTemplate = require('./template')
 const headerMiddleware = require('../header')
 const axios = require('axios')
 
-Page('/', headerMiddleware, loadPictures, (context, next) => {
+Page('/', headerMiddleware, loadPicturesFetch, (context, next) => {
 
   // Usando jQuery para cambiar el titulo de la pagina
   $('title').html('Platzigram')
@@ -14,8 +14,11 @@ Page('/', headerMiddleware, loadPictures, (context, next) => {
   main.empty().append(homepageTemplate(context.pictures))
 })
 
-// Middleware para cargar las fotos, antes de cargar el template de homepage
-function loadPictures(context, next) {
+// Middleware para cargar las fotos usando Axios, antes de cargar el template de homepage
+function loadPicturesAxios(context, next) {
+  
+  console.log('Entra con Axios')
+  
   axios
     // Envia la solicitud de las fotos a la API
     .get('/api/pictures')
@@ -28,4 +31,24 @@ function loadPictures(context, next) {
     .catch(function (err) {
       console.log(err)
     })
+}
+
+// Middleware para cargar las fotos usando Fetch, antes de cargar el template de homepage
+function loadPicturesFetch(context, next) {
+  
+  console.log('Entra con Fetch')
+  // Envia la solicitud de las fotos a la API
+  fetch('/api/pictures')
+  .then(function (res) {
+    return res.json()
+  })
+  .then(function (pics) {
+    // El contexto de los middlewares se utiliza para compartir informacion entre ellos
+    context.pictures = pics
+    // Llama al siguiente middleware
+    next()
+  })
+  .catch(function (err) {
+    console.log(err)
+  })
 }
